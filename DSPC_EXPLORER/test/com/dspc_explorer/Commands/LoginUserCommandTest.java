e179f6d87f6f0ae820cdb13c326b2c532e05a67d
DESKTOP-FIE7QI7\Asare <gohan.david11@gmail.com>
2018-02-07 22:36:20 +0000

    Test Package and LoginCommand Test class

---
+++ DSPC_EXPLORER/test/com/dspc_explorer/Commands/LoginUserCommandTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dspc_explorer.Commands;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Asare
+ */
+public class LoginUserCommandTest {
+    
+    public LoginUserCommandTest() {
+    }
+
+    /**
+     * Test of execute method, of class LoginUserCommand.
+     */
+    @Test
+    public void testExecute() {
+    }
+    
+}
